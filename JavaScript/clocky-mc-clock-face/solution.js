a8467d6a98eb6c8c7e152b07c3fd30f82818bdc0
Jon Sanders <jonsanders101@aol.com>
2018-03-11 11:37:20 +0000

    Add clocky-mc-clockface solution

---
+++ JavaScript/clocky-mc-clock-face/solution.js
@@ -0,0 +1,15 @@
+var whatTimeIsIt = function(angle) {
+    const fraction = angle / 360;
+    if (fraction === 0 || fraction === 1) {
+      return '12:00';
+    }
+    const minutesPastTwelve = Math.floor(12 * 60 * fraction);
+    const hours = Math.floor(minutesPastTwelve / 60);
+    const minutes = minutesPastTwelve - (hours * 60);
+    return `${formatTime(hours)}:${formatTime(minutes)}`;
+  }
+  
+  var formatTime = function(time) {
+    const formattedTime = time.toString()
+    return formattedTime.length === 1 ? '0' + formattedTime : formattedTime;
+  }
