14723b183670fdb85b2f2a022dd531ca44861cb3
Jon Sanders <jonsanders101@aol.com>
2018-03-11 15:30:55 +0000

    Refactor solution

--- JavaScript/clocky-mc-clock-face/solution.js
+++ JavaScript/clocky-mc-clock-face/solution.js
@@ -1,12 +1,14 @@
 const whatTimeIsIt = function(angle) {
     const fraction = angle / 360;
+    const totalMinutes = 720;
     
-    if (fraction === 0 || fraction === 1) {
+    if (fraction === 0) {
       return '12:00';
     }
-    const minutesPastTwelve = Math.floor(12 * 60 * fraction);
+    const minutesPastTwelve = Math.floor(totalMinutes * fraction);
     const hours = Math.floor(minutesPastTwelve / 60);
-    const minutes = minutesPastTwelve - (hours * 60);
+    const minutes = minutesPastTwelve - hours * 60;
+
     return `${formatTime(hours)}:${formatTime(minutes)}`;
   }
   
