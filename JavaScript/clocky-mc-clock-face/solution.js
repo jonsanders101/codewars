581067c38843d7dd49747475de05844cf0305172
Jon Sanders <jonsanders101@aol.com>
2018-03-11 16:24:22 +0000

    Refactor

--- JavaScript/clocky-mc-clock-face/solution.js
+++ JavaScript/clocky-mc-clock-face/solution.js
@@ -1,20 +1,20 @@
-const whatTimeIsIt = function(angle) {
-    const fraction = angle / 360;
-    const totalMinutes = 720;
-    
-    if (fraction === 0) {
-      return '12:00';
-    }
-    const minutesPastTwelve = Math.floor(totalMinutes * fraction);
-    const hours = Math.floor(minutesPastTwelve / 60);
-    const minutes = minutesPastTwelve - hours * 60;
+const whatTimeIsIt = (angle) => {
+  const fraction = angle / 360;
+  const totalMinutes = 720;
+  const minutesPastTwelve = Math.floor(totalMinutes * fraction);
+  const hours = Math.floor(minutesPastTwelve / 60);
+  const minutes = minutesPastTwelve - hours * 60;
 
-    return `${formatTime(hours)}:${formatTime(minutes)}`;
-  }
-  
-  const formatTime = function(time) {
-    const formattedTime = time.toString()
+  return formatTime(hours, minutes);
+};
+
+const formatTime = (hours, minutes) => {
+  hours = hours === 0 ? 12 : hours;
+
+  return [hours, minutes].map(time => {
+    let formattedTime = time.toString();
     return formattedTime.length === 1 ? '0' + formattedTime : formattedTime;
-  }
+  }).join(':');
+};
 
-  module.exports = whatTimeIsIt;
+module.exports = whatTimeIsIt;
