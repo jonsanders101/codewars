0c3272a2f1b2b13ebefabe765d2a2ee467c34820
Jon Sanders <jonsanders101@aol.com>
2018-06-24 22:24:02 +0100

    Refactor land perimeter kata

--- JavaScript/kata/land-perimeter/solution.js
+++ JavaScript/kata/land-perimeter/solution.js
@@ -1,5 +1,5 @@
-export default function landPerimeter(arr) {
-    var perimeter = 0;
+const landPerimeter = arr => {
+  let perimeter = 0;
     for (let x = 0; x < arr.length; x++) {
       for (let y = 0; y < arr[x].length; y++) {
         if (arr[x][y] === "X") {
@@ -8,29 +8,29 @@ export default function landPerimeter(arr) {
           } else {
             if (arr[x-1][y] !== "X") {
               perimeter++;
-            }
-          }
+            };
+          };
           if (arr[x+1] === undefined) {
             perimeter++;
           } else {
             if (arr[x+1][y] !== "X") {
               perimeter++;
-            }
-          }
+            };
+          };
           if (arr[x] === undefined) {
             perimeter++;
           } else {
             if (arr[x][y-1] !== "X") {
               perimeter++;
-            }
+            };
             if (arr[x][y+1] !== "X") {
               perimeter++;
-            }
-          }
-        }
-      }
-    }
-    return `Total land perimeter: ${perimeter}`;
-    }
+            };
+          };
+        };
+      };
+    };
+  return `Total land perimeter: ${perimeter}`;
+};
 
-    console.log(landPerimeter(["OXOOOX", "OXOXOO", "XXOOOX", "OXXXOO", "OOXOOX", "OXOOOO", "OOXOOX", "OOXOOO", "OXOOOO", "OXOOXX"]));
+export default landPerimeter;
