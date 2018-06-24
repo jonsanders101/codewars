c5c23c895a25dca4f3748b7346eb37595c7f873d
Jon Sanders <jonsanders101@aol.com>
2018-06-24 16:36:02 +0100

    Add Number Pyramid Kata

---
+++ JavaScript/kata/number-pyramid/solution.js
@@ -0,0 +1,34 @@
+function whiteSpace(bottomRow, middleNumbers) {
+    var output = '';
+      for (let i = 0; i < (bottomRow - middleNumbers.length)/2; i++) {
+        output = output.concat(' ');
+      }
+    return output;
+  }
+  
+  function numbers(n) {
+    var output = '';
+      for (let i = 1; i <= n; i++) {
+        output = output.concat(i);
+      };
+    output = output.concat(output.slice(0, output.length - 1).split('').reverse().join(''));
+    return output;
+  }
+
+export default function pattern(n){
+    var output="";
+    if (n <= 0) {
+        return output;
+    }
+
+    var finalRow = numbers(n);
+    for (let i = 1; i <= n; i++) {
+        var middleNumbers = numbers(i);
+        var whiteSpaceString = whiteSpace(finalRow.length, middleNumbers);
+        output = output.concat(whiteSpaceString, middleNumbers, whiteSpaceString);
+        if (i < n) {
+        output = output.concat('\n');
+        };
+    };
+    return output;
+};
---
+++ JavaScript/kata/number-pyramid/spec.js
@@ -0,0 +1,11 @@
+import pattern from './solution';
+
+fdescribe('Pattern', () => {
+    it('should return the correct pattern', () => {
+        expect(pattern(7)).toEqual("      1      \n     121     \n    12321    \n   1234321   \n  123454321  \n 12345654321 \n1234567654321");
+        expect(pattern(1)).toEqual("1");
+        expect(pattern(4)).toEqual("   1   \n  121  \n 12321 \n1234321");
+        expect(pattern(0)).toEqual("");
+        expect(pattern(-25)).toEqual("");
+    });
+});
